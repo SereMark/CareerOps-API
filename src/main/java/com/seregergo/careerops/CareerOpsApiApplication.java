06d91af22cd28077932f141bec8a821370c5d4fa
SereMark <126908754+SereMark@users.noreply.github.com>
2026-06-21 10:24:20 +0200

    init

---
+++ src/main/java/com/seregergo/careerops/CareerOpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.seregergo.careerops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerOpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerOpsApiApplication.class, args);
+	}
+
+}
